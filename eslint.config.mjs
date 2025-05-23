d3831c90a0b3a6c8f37f1cc055957c35013564c5
olehkutsyk <oleh.kutsyk@group107.com>
2025-05-23 12:22:15 +0300

    add basic ESLint and prettier configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
+import prettierPlugin from 'eslint-plugin-prettier';
+import unicornPlugin from 'eslint-plugin-unicorn';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +12,100 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ),
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+      unicorn: unicornPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'no-duplicate-imports': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-alert': 'error',
+      'no-eval': 'error',
+      'unicorn/custom-error-definition': 'error',
+      'unicorn/empty-brace-spaces': 'error',
+      'unicorn/prefer-includes': 'error',
+      '@typescript-eslint/no-shadow': ['error'],
+      '@typescript-eslint/no-explicit-any': ['error'],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'no-multi-spaces': [
+        'error',
+        {
+          // disallow multiple spaces
+          exceptions: {
+            Property: true,
+            VariableDeclarator: true,
+            ImportDeclaration: true,
+          },
+        },
+      ],
+      'jsx-quotes': ['error', 'prefer-single'],
+      'max-len': [
+        // enforce a maximum line length
+        'error',
+        {
+          code: 140,
+          tabWidth: 2,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreComments: true,
+        },
+      ],
+      quotes: [
+        // enforce the consistent use of either backticks, double, or single quotes
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: false,
+        },
+      ],
+      'no-magic-numbers': [
+        'error',
+        {
+          ignore: [0, 1, 2, -1],
+          ignoreArrayIndexes: true,
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
 ];
-
-export default eslintConfig;
---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+};
